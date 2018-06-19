ed3e0034d5bab4439096a8363d0e878b7fb230fb
FelipeTiberio <felipetiberio@yahoo.com.br>
2018-06-19 14:29:39 -0300

    Fiz os drestruidores das tads

--- include/listacircular.h
+++ include/listacircular.h
@@ -58,6 +58,19 @@ namespace myTads{
 	template <typename T>
 	list<T>::~list() 
 	{
+		
+		auto atual = this->cauda->getNext(); // primeiro elemento 
+		int posAtual = 0;
+
+		while ( posAtual != tamanho-1) { // percorre a lista até  o elemento com a posição passada como argumento
+			cauda->setNext(atual->getNext()->getNext()); 
+			atual = atual->getNext();
+			posAtual++;
+			cout << posAtual << "** \n";
+		}
+		this->cauda->setNext(nullptr);
+		cout << "tamanho = " << tamanho << "\n";
+		tamanho =0;
 
 	}
 
